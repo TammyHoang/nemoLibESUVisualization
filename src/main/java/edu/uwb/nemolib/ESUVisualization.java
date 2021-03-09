84f9d2f5faa8fc0e4f0cc4009c3741d9de9dd590
TammyHoang <66705113+TammyHoang@users.noreply.github.com>
2021-03-09 02:31:01 -0800

    Combined both GUIs into one

--- src/main/java/edu/uwb/nemolib/ESUVisualization.java
+++ src/main/java/edu/uwb/nemolib/ESUVisualization.java
@@ -2,6 +2,10 @@ package edu.uwb.nemolib;
 
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import org.jgrapht.*;
 import org.jgrapht.ext.*;
 import org.jgrapht.graph.*;
@@ -9,6 +13,7 @@ import org.jgrapht.graph.*;
 import javax.swing.*;
 import java.util.Map;
 import java.util.stream.Stream;
+import javax.swing.filechooser.FileSystemView;
 
 /**
  *
@@ -17,25 +22,128 @@ import java.util.stream.Stream;
 public class ESUVisualization extends JFrame {
 
     private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
-    private int levels;
+    private static Integer motifSize;
+    private static String inputFileName;
+    
+    // Variables for user input GUI
+    private static JLabel lblFile;
+    private static JLabel lblPrompt;
+    private static JButton btnOpen;
+    private static JButton btnSubmit;
+    private static JTextField txtMotifSize;
+    private static JLabel lblMotifSize;
 
     /**
      * The default constructor for ESUVisualization class.
      *
      * @param title the title of the JFrame.
-     * @param graph
-     * @param motifSize
      */
-    public ESUVisualization(String title, Graph graph, int motifSize) {
+    public ESUVisualization(String title) {
         super(title);
-        levels = motifSize;
-        addComponents(graph);
+        addComponents();
     }
 
     /**
      * Private method used to add components to the frame.
      */
-    private void addComponents(Graph graph) {
+    private void addComponents() {
+        JPanel pnlFile = new JPanel();
+        lblPrompt = new JLabel("Select a graph text file.");
+        pnlFile.add(lblPrompt);
+        btnOpen = new JButton("Open");
+        pnlFile.add(btnOpen);
+        lblFile = new JLabel("No file selected");
+        pnlFile.add(lblFile);
+        lblFile.setText("");
+
+        // Input motif size component
+        txtMotifSize = new JTextField(6);
+        lblMotifSize = new JLabel("Enter an integer motif size (motif size must be 3 or larger):");
+
+        // Submit button
+        btnSubmit = new JButton("Submit");
+        
+        JPanel pnlMotifSize = new JPanel();
+        
+        pnlMotifSize.add(lblMotifSize);
+        pnlMotifSize.add(txtMotifSize);
+        pnlMotifSize.add(btnSubmit);
+
+        JPanel pnlNorth = new JPanel();
+        pnlNorth.setLayout(new BoxLayout(pnlNorth, BoxLayout.Y_AXIS));
+        pnlNorth.add(pnlFile);
+        pnlNorth.add(pnlMotifSize);
+        
+        getContentPane().add(pnlNorth, BorderLayout.NORTH);
+        addEventHandlers();
+    }
+
+    // Controls the event of when the "Open" and "Submit" buttons are clicked
+    private void addEventHandlers() {
+        // Open button
+        btnOpen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser file = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+                int r = file.showOpenDialog(null);
+                if (r == JFileChooser.APPROVE_OPTION) {
+                    inputFileName = file.getSelectedFile().getAbsolutePath();
+                    lblFile.setText(inputFileName);
+                } else {
+                    lblFile.setText("User cancelled operation");
+                }
+            }
+        });
+        
+        // Submit button
+        // After submitting, the ESU algorithm executes to display the graph
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                String motifSizeInput = txtMotifSize.getText();
+                txtMotifSize.setText("");
+                
+                // Returns if the input file does not exist
+                if (inputFileName == null) {
+                    return;
+                }
+
+                // Check if the user input is an integer
+                try {
+                    motifSize = Integer.valueOf(motifSizeInput); 
+                } catch (NumberFormatException ex) {
+                    return;
+                }
+                
+                // Ensures that the motif size is at least 3
+                if (motifSize < 3) {
+                    return;
+                }
+                
+                lblMotifSize.setText("Enter a motif size (motif size must be 3 or larger):");
+                String filename = inputFileName;
+                System.out.println("filename = " + inputFileName);
+                boolean directed = false;
+
+                // Parse input graph
+                System.out.println("Parsing target graph...");
+                Graph targetGraph = null;
+
+                try {
+                    targetGraph = GraphParser.parse(filename, directed);
+                } catch (IOException ex) {
+                    System.err.println("Could not process " + filename);
+                    System.err.println(ex);
+                    System.exit(-1);
+                }
+
+                addESUTree(targetGraph, motifSize);
+            }
+        });
+    }
+    
+    private void addESUTree(Graph graph, Integer motifSize){
         // create a JGraphT graph
         ListenableGraph<String, DefaultEdge> g
                 = new DefaultListenableGraph<>(new SimpleGraph<>(DefaultEdge.class));
@@ -44,7 +152,7 @@ public class ESUVisualization extends JFrame {
         jgxAdapter = new JGraphXAdapter<>(g);
 
         mxGraphComponent component = new mxGraphComponent(jgxAdapter);
-        getContentPane().add(component);
+        getContentPane().add(component, BorderLayout.CENTER);
 
         g.addVertex("Root");
 
@@ -55,6 +163,7 @@ public class ESUVisualization extends JFrame {
 
             System.out.println("Lvl1Parent: " + Integer.valueOf(key.toString()));
             AdjacencyList children = graph.getAdjacencyList((Integer) graph.getNameToIndexMap().get(key));
+            
             // Iterating through each vertices' children
             CompactHashSet.Iter i = children.iterator();
             while (i.hasNext()) {
@@ -63,15 +172,18 @@ public class ESUVisualization extends JFrame {
                 path.add((Integer) graph.getNameToIndexMap().get(key));
                 path.add(curr);
                 String vertex = key.toString();
+                
                 // Getting the key (node) from the key-value HashMap
                 Stream<String> keyStream = keys(graph.getNameToIndexMap(), curr);
                 Integer child = Integer.valueOf(keyStream.findFirst().get());
+                
                 // Check if the child is greater than the parent
                 if (child > Integer.valueOf(key.toString())) {
                     vertex += "," + child.toString();
                     g.addVertex(vertex);
                     g.addEdge(key.toString(), vertex);
                     System.out.println("\tLvl2Child=" + child);
+                    
                     // Find the children's children
                     AdjacencyList nextLvlChildren = new AdjacencyList();
                     // Get the siblings
@@ -82,6 +194,7 @@ public class ESUVisualization extends JFrame {
                         // Get the key (node) from the key-value HashMap
                         keyStream = keys(graph.getNameToIndexMap(), sIter);
                         Integer sibling = Integer.valueOf(keyStream.findFirst().get());
+                        
                         // Check if the sibling is greater than the current node
                         if (sibling > child) {
                             nextLvlChildren.add(sIter);
@@ -90,6 +203,7 @@ public class ESUVisualization extends JFrame {
                     // Get the object from the key-value HashMap
                     Stream<Object> keyStream1 = keys(graph.getNameToIndexMap(), curr);
                     Object key1 = keyStream1.findFirst().get();
+                    
                     // Get the adjacency list's iterator of the current object
                     CompactHashSet.Iter iNextLvlChildren = graph.getAdjacencyList(
                             (Integer) graph.getNameToIndexMap().get(key1)).iterator();
@@ -98,6 +212,7 @@ public class ESUVisualization extends JFrame {
                         // Get the key (node) from the key-value HashMap
                         keyStream = keys(graph.getNameToIndexMap(), nextLevelChild);
                         child = Integer.valueOf(keyStream.findFirst().get());
+                        
                         // Check if the child is greater than the parent node and the siblings don't already contain the next level's child
                         if (child > Integer.valueOf(key.toString()) && !siblings.contains(nextLevelChild)) {
                             nextLvlChildren.add(nextLevelChild);
@@ -109,6 +224,7 @@ public class ESUVisualization extends JFrame {
                         int nextChild = Math.abs(childrenIter.next());
                         AdjacencyList currPath = path.copy();
                         currPath.add(nextChild);
+                        
                         // Get the key (node) from the key-value HashMap
                         keyStream = keys(graph.getNameToIndexMap(), nextChild);
                         child = Integer.valueOf(keyStream.findFirst().get());
@@ -121,11 +237,11 @@ public class ESUVisualization extends JFrame {
             }
         }
 
-        // positioning via jgraphx layouts
+        // Creating a tree layout
         mxHierarchicalLayout layout = new mxHierarchicalLayout(jgxAdapter);
         layout.execute(jgxAdapter.getDefaultParent());
     }
-
+    
     public <K, V> Stream<K> keys(Map<K, V> map, V value) {
         return map
                 .entrySet()
@@ -133,4 +249,5 @@ public class ESUVisualization extends JFrame {
                 .filter(entry -> value.equals(entry.getValue()))
                 .map(Map.Entry::getKey);
     }
-            }
+    
+}
